7e694bbc1c35b47c3c89da219894126e6dfb6916
Figby222 <figby222@outlook.com>
2024-08-26 22:57:54 -0700

    Add GET request using https module

--- http-requests.cjs
+++ http-requests.cjs
@@ -1,4 +1,26 @@
 const axios = require("axios");
+const https = require("https");
+
+const options = {
+    hostname: "example.com",
+    port: 443,
+    path: "/cookies",
+    method: "GET",
+};
+
+const request = https.request(options, response => {
+    console.log(`statusCode: ${response.statusCode}`);
+
+    response.on('data', data => {
+        process.stdout.write(data);
+    });
+});
+
+request.on('error', error => {
+    console.log(error);
+});
+
+request.end();
 
 axios.get("https://blahblahblah.com")
     .then(response => response.JSON())
