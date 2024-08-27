c68cacc16c931f03c2c90006a56e9535af6d3351
Figby222 <figby222@outlook.com>
2024-08-26 23:08:15 -0700

    Add HTTP POST request using https module

--- http-requests.cjs
+++ http-requests.cjs
@@ -22,6 +22,36 @@ request.on('error', error => {
 
 request.end();
 
+const data = JSON.stringify({
+    cookies: [ "Sugar" ],
+});
+
+const options2 = {
+    hostname: "example.com",
+    port: 443,
+    path: "/cookies",
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Content-Length": data.length,
+    },
+};
+
+const request2 = https.request(options, response => {
+    console.log(`statusCode: ${response.statusCode}`);
+
+    response.on("data", data => {
+        process.stdout.write(data);
+    });
+});
+
+request2.on("error", error => {
+    console.log(error);
+});
+
+request2.write(data);
+request2.end();
+
 axios.get("https://blahblahblah.com")
     .then(response => response.JSON())
     .then(response => console.log(response))
